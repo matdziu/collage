754dd8640c978c8b5a893cf345d86d2015fe81a9
Mateusz Dziubek <mateusz.dziubek@schibsted.pl>
2016-12-18 17:41:06 +0100

    Implemented method for immersive mode

--- app/src/main/java/com/collage/MainActivity.java
+++ app/src/main/java/com/collage/MainActivity.java
@@ -1,7 +1,7 @@
 package com.collage;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/collage/display/DisplayFragment.java
+++ app/src/main/java/com/collage/display/DisplayFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
@@ -31,6 +30,7 @@ public class DisplayFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_display, container, false);
         ButterKnife.bind(this, view);
+        hideSystemUI();
 
         List<Photo> photoList = new ArrayList<>();
         photoList.add(new Photo(ContextCompat.getColor(getContext(), R.color.colorAccent)));
@@ -43,5 +43,21 @@ public class DisplayFragment extends Fragment {
         recyclerView.setAdapter(new PhotosAdapter(photoList));
 
         return view;
+    }
+
+    private View getDecorView() {
+        return getActivity()
+                .getWindow()
+                .getDecorView();
+    }
+
+    private void hideSystemUI() {
+        getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
     }
 }
