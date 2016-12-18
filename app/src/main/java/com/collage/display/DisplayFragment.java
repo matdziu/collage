2cc64f92b8ef48583dd162e900770000f6536f8a
Mateusz Dziubek <mateusz.dziubek@schibsted.pl>
2016-12-18 19:54:13 +0100

    No randomisation, now properly resizing width of images

--- app/src/main/java/com/collage/display/DisplayFragment.java
+++ app/src/main/java/com/collage/display/DisplayFragment.java
@@ -1,12 +1,13 @@
 package com.collage.display;
 
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
-import android.util.TypedValue;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,9 +17,7 @@ import com.collage.model.Photo;
 import com.collage.util.PhotosAdapter;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -28,57 +27,30 @@ public class DisplayFragment extends Fragment {
     @BindView(R.id.photos_recycler_view)
     RecyclerView recyclerView;
 
-    private List<Photo> photoList;
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        photoList = new ArrayList<>();
-
-        // height and width are supplied here in dp, but layoutParams in adapter take only px hence conversion
-        photoList.add(new Photo(ContextCompat.getDrawable(getContext(), R.drawable.sample_photo),
-                convertToPx(randomiseWidth())));
-        photoList.add(new Photo(ContextCompat.getDrawable(getContext(), R.drawable.sample_photo),
-                convertToPx(randomiseWidth())));
-        photoList.add(new Photo(ContextCompat.getDrawable(getContext(), R.drawable.sample_photo),
-                convertToPx(randomiseWidth())));
-        photoList.add(new Photo(ContextCompat.getDrawable(getContext(), R.drawable.sample_photo),
-                convertToPx(randomiseWidth())));
-        photoList.add(new Photo(ContextCompat.getDrawable(getContext(), R.drawable.sample_photo),
-                convertToPx(randomiseWidth())));
-        photoList.add(new Photo(ContextCompat.getDrawable(getContext(), R.drawable.sample_photo),
-                convertToPx(randomiseWidth())));
-        photoList.add(new Photo(ContextCompat.getDrawable(getContext(), R.drawable.sample_photo),
-                convertToPx(randomiseWidth())));
-        photoList.add(new Photo(ContextCompat.getDrawable(getContext(), R.drawable.sample_photo),
-                convertToPx(randomiseWidth())));
-        photoList.add(new Photo(ContextCompat.getDrawable(getContext(), R.drawable.sample_photo),
-                convertToPx(randomiseWidth())));
-        photoList.add(new Photo(ContextCompat.getDrawable(getContext(), R.drawable.sample_photo),
-                convertToPx(randomiseWidth())));
-        photoList.add(new Photo(ContextCompat.getDrawable(getContext(), R.drawable.sample_photo),
-                convertToPx(randomiseWidth())));
-        photoList.add(new Photo(ContextCompat.getDrawable(getContext(), R.drawable.sample_photo),
-                convertToPx(randomiseWidth())));
-        photoList.add(new Photo(ContextCompat.getDrawable(getContext(), R.drawable.sample_photo),
-                convertToPx(randomiseWidth())));
-        photoList.add(new Photo(ContextCompat.getDrawable(getContext(), R.drawable.sample_photo),
-                convertToPx(randomiseWidth())));
-        photoList.add(new Photo(ContextCompat.getDrawable(getContext(), R.drawable.sample_photo),
-                convertToPx(randomiseWidth())));
-        photoList.add(new Photo(ContextCompat.getDrawable(getContext(), R.drawable.sample_photo),
-                convertToPx(randomiseWidth())));
-    }
-
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_display, container, false);
         ButterKnife.bind(this, view);
 
-        // for testing purposes!
-        setRetainInstance(true);
+        List<Photo> photoList = new ArrayList<>();
+
+        photoList.add(new Photo(ContextCompat.getDrawable(getContext(), R.drawable.sample_photo), getProperWidth()));
+        photoList.add(new Photo(ContextCompat.getDrawable(getContext(), R.drawable.sample_photo), getProperWidth()));
+        photoList.add(new Photo(ContextCompat.getDrawable(getContext(), R.drawable.sample_photo), getProperWidth()));
+        photoList.add(new Photo(ContextCompat.getDrawable(getContext(), R.drawable.sample_photo), getProperWidth()));
+        photoList.add(new Photo(ContextCompat.getDrawable(getContext(), R.drawable.sample_photo), getProperWidth()));
+        photoList.add(new Photo(ContextCompat.getDrawable(getContext(), R.drawable.sample_photo), getProperWidth()));
+        photoList.add(new Photo(ContextCompat.getDrawable(getContext(), R.drawable.sample_photo), getProperWidth()));
+        photoList.add(new Photo(ContextCompat.getDrawable(getContext(), R.drawable.sample_photo), getProperWidth()));
+        photoList.add(new Photo(ContextCompat.getDrawable(getContext(), R.drawable.sample_photo), getProperWidth()));
+        photoList.add(new Photo(ContextCompat.getDrawable(getContext(), R.drawable.sample_photo), getProperWidth()));
+        photoList.add(new Photo(ContextCompat.getDrawable(getContext(), R.drawable.sample_photo), getProperWidth()));
+        photoList.add(new Photo(ContextCompat.getDrawable(getContext(), R.drawable.sample_photo), getProperWidth()));
+        photoList.add(new Photo(ContextCompat.getDrawable(getContext(), R.drawable.sample_photo), getProperWidth()));
+        photoList.add(new Photo(ContextCompat.getDrawable(getContext(), R.drawable.sample_photo), getProperWidth()));
+        photoList.add(new Photo(ContextCompat.getDrawable(getContext(), R.drawable.sample_photo), getProperWidth()));
+        photoList.add(new Photo(ContextCompat.getDrawable(getContext(), R.drawable.sample_photo), getProperWidth()));
 
         recyclerView.setLayoutManager(new StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL));
         recyclerView.setAdapter(new PhotosAdapter(photoList));
@@ -108,15 +80,13 @@ public class DisplayFragment extends Fragment {
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
     }
 
-    private int convertToPx(int dp) {
-        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp,
-                getResources().getDisplayMetrics());
-    }
+    private int getProperWidth() {
+        Display display = getActivity().
+                getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
 
-    private int randomiseWidth() {
-        List<Integer> acceptable = new ArrayList<>(Arrays.asList
-                (80, 120, 150, 240));
-        return acceptable.get(new Random().nextInt(acceptable.size()));
+        return (size.x / 3);
     }
 
 }
