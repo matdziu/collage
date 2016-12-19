0b8ed53be3e06da92fbd629b1b9f3eb7d0966e82
Mateusz Dziubek <mateusz.dziubek@schibsted.pl>
2016-12-19 13:00:17 +0100

    Implemented surfaceListener now moving to setUpCamera method

--- app/src/main/java/com/collage/camera/CameraFragment.java
+++ app/src/main/java/com/collage/camera/CameraFragment.java
@@ -1,17 +1,31 @@
 package com.collage.camera;
 
+import android.graphics.SurfaceTexture;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.collage.R;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class CameraFragment extends Fragment {
+
+    @BindView(R.id.texture_view)
+    TextureView textureView;
+
+    private TextureView.SurfaceTextureListener surfaceTextureListener;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        surfaceTextureListener = setSurfaceTextureListener();
+    }
 
     @Nullable
     @Override
@@ -26,6 +40,12 @@ public class CameraFragment extends Fragment {
     public void onResume() {
         super.onResume();
         hideSystemUI();
+
+        if (textureView.isAvailable()) {
+
+        } else {
+            textureView.setSurfaceTextureListener(surfaceTextureListener);
+        }
     }
 
     private View getDecorView() {
@@ -42,5 +62,33 @@ public class CameraFragment extends Fragment {
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
+
+    private TextureView.SurfaceTextureListener setSurfaceTextureListener() {
+        return new TextureView.SurfaceTextureListener() {
+            @Override
+            public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int width, int height) {
+                setUpCamera(width, height);
+            }
+
+            @Override
+            public void onSurfaceTextureSizeChanged(SurfaceTexture surfaceTexture, int width, int height) {
+
+            }
+
+            @Override
+            public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) {
+                return false;
+            }
+
+            @Override
+            public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) {
+
+            }
+        };
+    }
+
+    private void setUpCamera(int width, int height) {
+
     }
 }
