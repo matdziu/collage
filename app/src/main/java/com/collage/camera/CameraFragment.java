0d8e649294d9ab8db7dba1f94c3611e4da61d97d
Mateusz Dziubek <mateusz.dziubek@schibsted.pl>
2016-12-19 10:58:59 +0100

    Added basic files for camera feature

---
+++ app/src/main/java/com/collage/camera/CameraFragment.java
@@ -0,0 +1,24 @@
+package com.collage.camera;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.collage.R;
+
+import butterknife.ButterKnife;
+
+public class CameraFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_camera, container, false);
+        ButterKnife.bind(this, view);
+
+        return view;
+    }
+}
