89483d8c4a1e6bbe2152a2bd0a3f9d1570cffcff
Mateusz Dziubek <mateusz.dziubek@schibsted.pl>
2016-12-19 12:27:51 +0100

    Added immersive mode to CameraFragment

--- app/src/main/java/com/collage/camera/CameraFragment.java
+++ app/src/main/java/com/collage/camera/CameraFragment.java
@@ -21,4 +21,26 @@ public class CameraFragment extends Fragment {
 
         return view;
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        hideSystemUI();
+    }
+
+    private View getDecorView() {
+        return getActivity()
+                .getWindow()
+                .getDecorView();
+    }
+
+    private void hideSystemUI() {
+        getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
 }
