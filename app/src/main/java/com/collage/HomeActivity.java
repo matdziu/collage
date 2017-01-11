dfb7d7f44a8b5b43fccb9155b63d5a51c904d729
Mateusz Dziubek <mateusz.dziubek@schibsted.pl>
2017-01-11 12:09:02 +0100

    Added toolbar

--- app/src/main/java/com/collage/HomeActivity.java
+++ app/src/main/java/com/collage/HomeActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.collage.util.HomeFragmentPagerAdapter;
 
@@ -18,11 +19,16 @@ public class HomeActivity extends AppCompatActivity {
     @BindView(R.id.home_view_pager)
     ViewPager homeViewPager;
 
+    @BindView(R.id.home_toolbar)
+    Toolbar homeToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         ButterKnife.bind(this);
+
+        setSupportActionBar(homeToolbar);
 
         HomeFragmentPagerAdapter homeFragmentPagerAdapter = new HomeFragmentPagerAdapter(getSupportFragmentManager());
         homeViewPager.setAdapter(homeFragmentPagerAdapter);
