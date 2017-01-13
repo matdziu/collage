9b65b6433b57665c90ef24b21c889b4967711648
Mateusz Dziubek <mateusz.dziubek@schibsted.pl>
2017-01-13 13:46:22 +0100

    Applied proper dimensions

--- app/src/main/java/com/collage/BaseFragment.java
+++ app/src/main/java/com/collage/BaseFragment.java
@@ -36,8 +36,6 @@ public class BaseFragment extends Fragment {
 
     protected void showSystemUI() {
         getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
     }
 }
