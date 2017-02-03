a3799a2f4c5aa7fecaa87abed23860be6b7738aa
Mateusz Dziubek <mateusz.dziubek@schibsted.pl>
2017-02-03 16:35:34 +0100

    Now loading images as bitmap on PhotosAdapter

--- app/src/main/java/com/collage/util/adapters/PhotosAdapter.java
+++ app/src/main/java/com/collage/util/adapters/PhotosAdapter.java
@@ -2,6 +2,7 @@ package com.collage.util.adapters;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,7 +11,6 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.resource.drawable.GlideDrawable;
 import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.Target;
 import com.collage.R;
@@ -48,24 +48,27 @@ public class PhotosAdapter extends RecyclerView.Adapter<PhotosAdapter.ViewHolder
     public void onBindViewHolder(final ViewHolder holder, int position) {
         setImageViewBounds(holder.imageView, imageWidth);
         holder.progressBar.setVisibility(View.VISIBLE);
+
         Glide.with(context)
                 .load(photoList.get(position).imageUrl)
-                .listener(new RequestListener<String, GlideDrawable>() {
+                .asBitmap()
+                .listener(new RequestListener<String, Bitmap>() {
                     @Override
-                    public boolean onResourceReady(GlideDrawable resource, String model,
-                                                   Target<GlideDrawable> target, boolean isFromMemoryCache,
+                    public boolean onResourceReady(Bitmap resource, String model,
+                                                   Target<Bitmap> target, boolean isFromMemoryCache,
                                                    boolean isFirstResource) {
                         holder.progressBar.setVisibility(View.GONE);
                         return false;
                     }
 
                     @Override
-                    public boolean onException(Exception e, String model,
-                                               Target<GlideDrawable> target, boolean isFirstResource) {
+                    public boolean onException(Exception e, String model, Target<Bitmap> target,
+                                               boolean isFirstResource) {
                         return false;
                     }
                 })
                 .into(holder.imageView);
+
         holder.imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
